794def21ceed90719c84ef145891b0b8cbababbf
Mika Nokka <mika.nokka@ambientia.fi>
2021-06-16 09:50:47 +0300

    Using old issue.store() can change Resolved Date as a date when issue entered to fixed status (DOne). This can be seen then in UI. Just state change saving

--- SetResolution.groovy
+++ SetResolution.groovy
@@ -15,11 +15,14 @@ import java.text.DateFormat
 import java.util.Date
 import groovy.time.TimeCategory
 import java.sql.Timestamp
+import com.atlassian.jira.event.type.EventDispatchOption
+import com.atlassian.jira.user.ApplicationUser
+
 
 
 // CONFIGURATIONS *********************
 
-int MonthsValue = 10 as Integer 
+int MonthsValue = 20 as Integer 
 
 // END OF CONFIGURATIONS ****************
 
@@ -33,10 +36,13 @@ mylogger.setLevel(Level.DEBUG ) // or INFO (prod) or DEBUG (development)
 mylogger.info("----------------STARTED--------------------------------------------------------------------")
 
 
-
+def issueService = ComponentAccessor.issueService
 def issueManager = ComponentAccessor.getIssueManager()
-MutableIssue myIssue = issueManager.getIssueObject("TESDATERES-1")
 
+def changeHistoryManager = ComponentAccessor.getChangeHistoryManager()
+
+MutableIssue myIssue = issueManager.getIssueObject("TESDATERES-1")
+def currentUser = ComponentAccessor.jiraAuthenticationContext.loggedInUser
 
 Date CurrentDate=new Date()
 mylogger.debug( "CurrentDate: $CurrentDate   ")
@@ -51,10 +57,42 @@ mylogger.debug( "New date: $timepast   ")
 mylogger.debug( "New datestamp: $PastDate   ")
 
 
+// check when issuw was acttually resolved, in this case when moved to "Done" state
+def done = changeHistoryManager.getChangeItemsForField(myIssue, "status").reverse().find {
+	it.toString == "Done"
+	}?.getCreated()
+def doneTime = done?.getTime()
+def doneDate
+doneDate = new Date((long)doneTime)
+
+def doneStamp
+def timedone
+use(TimeCategory) {
+	timedone=doneDate
+	doneStamp=new Timestamp(timedone.getTime())
+}
+
+mylogger.debug( "doneTime: $doneTime   ")
+mylogger.debug( "doneDate: $doneDate   ")
+mylogger.debug( "doneStamp: $doneStamp   ")
 
 //Timestamp time = new Timestamp(119,7,5,13,41,0,0)
-myIssue.setResolutionDate(PastDate)
-myIssue.store()
+//myIssue.setResolutionDate(PastDate)
+myIssue.setResolutionDate(doneStamp)
+
+
+//def issueInputParams = issueService.newIssueInputParameters()
+//issueInputParams.setResolutionDate(PastDate.toString())
+
+//issueManager.updateIssue(currentUser, myIssue, EventDispatchOption.ISSUE_UPDATED, false)
+myIssue.store() //works but usage not recommended anymore, the modern approach commented out does not work yet
+
+//def updateValidationResult = issueService.validateUpdate(currentUser, myIssue.id, issueInputParams)
+//assert updateValidationResult.valid : updateValidationResult.errorCollection
+
+//def updateResult = issueService.update(currentUser, updateValidationResult, EventDispatchOption.ISSUE_UPDATED, false)
+//assert updateResult.valid : updateResult.errorCollection
+
 
 
 mylogger.info("----------------ENDED--------------------------------------------------------------------")
